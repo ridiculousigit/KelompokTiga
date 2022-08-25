2e21949adb67b0f3ef1cc2e7e6034aaf4a5d91df
maulana421 <maulanaibnuf@gmaile.com>
2022-08-25 19:57:42 +0700

    layout_lana_satu

---
+++ app/src/main/java/binar/academy/kelompoktiga/lana_activity.kt
@@ -0,0 +1,11 @@
+package binar.academy.kelompoktiga
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class lana_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_lana)
+    }
+}
